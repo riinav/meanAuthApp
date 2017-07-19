f0b4f379bc46779ebfee8000568e40dc81cbf116
Rinav Gangar <rinav4all@gmail.com>
2017-07-19 19:35:35 +0530

    add app.js and bootstrap it

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// initialize our node app with Express
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('Server started on port ' + port);
+});
