78a03b7b62ef9f031a91196f2b0766dfc4afa1e5
Rinav Gangar <rinav4all@gmail.com>
2017-07-19 19:42:03 +0530

    add root route with invalid response

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('invalid Endpoint');
+});
+
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
