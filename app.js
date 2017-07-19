bd37ed490ec11439e3ba5f21ef78837ffb7eacea
Rinav Gangar <rinav4all@gmail.com>
2017-07-20 00:56:04 +0530

    attach CORS and body-parser to app

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ const mongoose = require('mongoose');
 // initialize our node app with Express
 const app = express();
 
+// port number
 const port = 3000;
 
+// CORS middleware
+app.use(cors());
+
+// bodyParser middleware
+app.use(bodyParser.json());
+
+// index route
 app.get('/', (req, res) => {
   res.send('invalid Endpoint');
 });
 
+// start server
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
